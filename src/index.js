c86eed7fc5ceb139ffa11c1c15555ebc96590fe3
Tahir Ahmad <tahirahmad.in@gmail.com>
2024-07-18 01:46:21 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { Buffer } from "buffer";
 
 window.Buffer = Buffer;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("draggable"));
 root.render(
   <BrowserRouter>
     <ThemeProvider theme={theme}>
